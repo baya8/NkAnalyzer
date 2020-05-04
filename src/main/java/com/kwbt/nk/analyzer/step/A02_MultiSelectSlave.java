b4b03b79dc03b6f878241df54edd0d7158e0eeef
m-kawabata <m-kawabata@rapide-act.com>
2020-05-05 03:11:26 +0900

    delete unuse method

--- src/main/java/com/kwbt/nk/analyzer/step/A02_MultiSelectSlave.java
+++ src/main/java/com/kwbt/nk/analyzer/step/A02_MultiSelectSlave.java
@@ -1,9 +1,5 @@
 package com.kwbt.nk.analyzer.step;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +13,6 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-import org.sqlite.SQLiteConfig;
-import org.sqlite.SQLiteConfig.JournalMode;
-import org.sqlite.SQLiteConfig.SynchronousMode;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kwbt.nk.analyzer.repo.SQLiteRepo;
@@ -118,61 +110,5 @@ public class A02_MultiSelectSlave implements Tasklet {
                 throw e;
             }
         }
-    }
-
-    private void run2(List<Parser> list) throws Exception {
-
-        String threadID = String.valueOf(Thread.currentThread().getId());
-
-        logger.info("start thread id: {}, list size is {}", threadID, list.size());
-
-        SQLiteConfig config = new SQLiteConfig();
-        config.setReadOnly(true);
-        config.setJournalMode(JournalMode.MEMORY);
-        config.setSynchronous(SynchronousMode.OFF);
-
-        try (Connection con = DriverManager.getConnection("jdbc:sqlite:Q:\\masataka\\syuto\\DB\\1.4\\race.db",
-                config.toProperties());) {
-
-            long s1 = System.currentTimeMillis();
-            for (int i = 0; i < list.size(); i++) {
-
-                long s2 = System.currentTimeMillis();
-                Parser p = list.get(i);
-                // PreparedStatement pstmt = con.prepareStatement(p.sql).executeQuery();
-                try (PreparedStatement pstmt = con.prepareStatement(p.sql);) {
-                    pstmt.setFetchSize(10000);
-                    ResultSet rs = pstmt.executeQuery();
-                    logger.info("time ss: {}", System.currentTimeMillis() - s2);
-
-                    long s3 = System.currentTimeMillis();
-                    while (rs.next()) {
-                        FeaturePayoff fp = new FeaturePayoff();
-                        // fp.setRace_id(rs.getInt("race_id"));
-                        // fp.setOrder_of_finish(rs.getInt("order_of_finish"));
-
-                        if (CollectionUtils.isEmpty(p.featurePayoffList)) {
-                            p.featurePayoffList = new ArrayList<>();
-                        }
-                        p.featurePayoffList.add(fp);
-                    }
-                    logger.info("time sss: {}", System.currentTimeMillis() - s3);
-                }
-
-                // logger.info("loop count: {}", p.featurePayoffList.size());
-
-                if (i % 10 == 0) {
-                    logger.info("【thread id {}】exec pass {}", threadID, i);
-                }
-            }
-
-        } catch (Exception e) {
-            throw e;
-        }
-
-        final ObjectMapper mapper = new ObjectMapper();
-
-        mapper.writeValueAsString(list);
-        fileUtil.writeFile(outputFileName + threadID, mapper.writeValueAsString(list));
     }
 }
